b355c5e75c96fa176de021c27ad6c0608dbc596e
dalareo <dalareo@gmail.com>
2014-08-12 16:43:41 +0200

    widgets no blogue

--- wp-content/themes/tema-modificado/functions.php
+++ wp-content/themes/tema-modificado/functions.php
@@ -34,3 +34,21 @@ add_action( 'init', 'register_my_menus' );
 // Register Custom Navigation Walker
 require_once('wp_bootstrap_navwalker.php');
 ?>
+<?php
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name' => 'Barra lateral blog',
+		'id' => 'blog_right_1',
+		'before_widget' => '<div>',
+		'after_widget' => '</div>',
+		'before_title' => '<h2 class="rounded">',
+		'after_title' => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+?>
