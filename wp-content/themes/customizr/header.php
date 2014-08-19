20288885e6bf6e3397f53dd581c63629b61a6b6c
Marta <sweena.z@gmail.com>
2014-08-19 17:25:39 +0200

    novo tema engadido

---
+++ wp-content/themes/customizr/header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The Header for Customizr.
+ *
+ * Displays all of the <head> section and everything up till <div id="main-wrapper">
+ *
+ * @package Customizr
+ * @since Customizr 1.0
+ */
+?>
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>                          
+<!--<![endif]-->
+
+	<?php 
+		//the '__before_body' hook is used by TC_header_main::$instance->tc_head_display()
+		do_action( '__before_body' ); 
+	?>
+
+	<body <?php body_class(); ?> <?php echo tc__f('tc_body_attributes' , 'itemscope itemtype="http://schema.org/WebPage"') ?>>
+		
+		<?php do_action( '__before_header' ); ?>
+
+	   	<header class="<?php echo tc__f('tc_header_classes', 'tc-header clearfix row-fluid') ?>" role="banner">
+			
+			<?php 
+			//the '__header' hook is used by (ordered by priorities) : TC_header_main::$instance->tc_logo_title_display(), TC_header_main::$instance->tc_tagline_display(), TC_header_main::$instance->tc_navbar_display()
+				do_action( '__header' ); 
+			?>
+
+		</header>
+
+		<?php 
+		 	//This hook is filtered with the slider : TC_slider::$instance->tc_slider_display()
+			do_action ( '__after_header' )
+		?>
