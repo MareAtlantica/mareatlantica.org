f87fcce0e8ef588c29360323b95e83666c07f21e
dalareo <dalareo@gmail.com>
2014-08-20 13:36:58 +0200

    modificado footer.php

---
+++ wp-content/themes/marea/footer.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * Default Footer
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage blank_bootstrap
+ * @since Blank Theme with Bootstrap 1.0
+ *
+ */
+// Gets footer
+?>
+<?php wp_footer(); ?>
+</div> <!-- // .container -->
+<div class="well well-sm">
+<a href="#top" class="pull-right">Inicio da páxina</a>
+#mareatlantica
+</div>
+
+</body>
+</html>
