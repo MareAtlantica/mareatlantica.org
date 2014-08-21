83cc312a91b4b28f3ebeb25d01b779c777207628
dalareo <dalareo@gmail.com>
2014-08-21 11:25:32 +0200

    incongruencias versiones Bootstrap y desactiva JS

--- wp-content/themes/marea/functions.php
+++ wp-content/themes/marea/functions.php
@@ -14,7 +14,7 @@ require( get_template_directory() . '/../marea/inc/custom-header.php' );
   // Includes Twitter Bootstrap.
 function wp_bootstrap_scripts_styles() {
   // Loads Bootstrap minified JavaScript file.
-  wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/../marea/js/bootstrap.min.js', array('jquery'),'3.0.0', true );
+//  wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/../marea/js/bootstrap.min.js', array('jquery'),'3.0.0', true );
   // Loads Bootstrap minified CSS file.
   wp_enqueue_style('bootstrapwp', get_template_directory_uri() . '/../marea/css/bootstrap.min.css', array( ), '3.0.0', false );
   // Loads holder.js.
